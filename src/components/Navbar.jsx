277d4adc84820192263692e1d24c38c0fe3d4c4f
Leon <leonissaclivi@gmail.com>
2025-05-21 23:33:51 +0530

    removed un necessary buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = () => {
             {isLoggedIn && (
               <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 z-30">
                 <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded shadow">
-                  <p className="cursor-pointer hover:text-black">My Profile</p>
+                  
                   <p
                     className="cursor-pointer hover:text-black"
                     onClick={() => navigate('/orders')}
@@ -102,9 +102,9 @@ const Navbar = () => {
           {/* Cart */}
           <Link to="/cart" className="relative">
             <img src={assets.cart_icon} className="w-5 min-w-5" alt="Cart" />
-            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            {/* <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
               5
-            </p>
+            </p> */}
           </Link>
 
           {/* Menu Icon for mobile */}
